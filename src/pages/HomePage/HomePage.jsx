7af5a9a938283baf7e360cf06bc0a44d58e41892
MSiesto <miguelsiesto@gmail.com>
2024-01-20 02:04:08 +0100

    implementing filtering systems

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,32 +1,77 @@
 import "./HomePage.css";
 import { useState, useEffect, useContext } from "react";
-import { Container } from "react-bootstrap";
-import { Button } from "react-bootstrap";
+import { Container, Dropdown } from "react-bootstrap";
 
 import gamesService from "../../services/games.services";
-
 import Loader from "../../components/Loader/Loader";
 import GamesList from "../../components/GameList/GameList";
-
 import { AuthContext } from "../../contexts/auth.context";
+import { Form } from "react-bootstrap";
 
 const HomePage = () => {
   const [games, setGames] = useState();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState(null);
+
+  console.log(searchTerm);
 
   useEffect(() => {
     loadGames();
-  }, []);
+  }, [sortOrder, searchTerm]);
 
   const loadGames = () => {
     gamesService
       .getAllGames()
-      .then(({ data }) => setGames(data))
+      .then(({ data }) => {
+        let filteredGames = data;
+
+        if (searchTerm) {
+          const searchTermLower = searchTerm.toLowerCase();
+          filteredGames = data.filter((game) =>
+            game.title.toLowerCase().includes(searchTermLower)
+          );
+        }
+
+        const sortedGames = filteredGames.slice();
+
+        if (sortOrder === "asc") {
+          sortedGames.sort((a, b) => a.votesReceived - b.votesReceived);
+        } else if (sortOrder === "desc") {
+          sortedGames.sort((a, b) => b.votesReceived - a.votesReceived);
+        }
+
+        setGames(sortedGames);
+      })
       .catch((err) => console.log(err));
   };
 
   const { user: userData } = useContext(AuthContext);
   const { availableVotes } = userData || {};
-  console.log(availableVotes);
+
+  const handleSortOrder = (order) => {
+    setSortOrder(order);
+  };
+
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value); // Manejar el cambio en el término de búsqueda mientras el usuario escribe
+  };
+
+  const renderDropdownButton = () => (
+    <Dropdown.Toggle variant="outline-primary" id="dropdown-basic">
+      Sort by Votes Received
+    </Dropdown.Toggle>
+  );
+
+  const renderDropdownItems = () => (
+    <>
+      <Dropdown.Item onClick={() => handleSortOrder("asc")}>
+        Less votes received first
+      </Dropdown.Item>
+      <Dropdown.Item onClick={() => handleSortOrder("desc")}>
+        More votes received first
+      </Dropdown.Item>
+    </>
+  );
 
   return (
     <>
@@ -41,6 +86,18 @@ const HomePage = () => {
             <h2>You have {availableVotes} votes left</h2>
           )}
         </div>
+        <Dropdown>
+          {renderDropdownButton()}
+          <Dropdown.Menu>{renderDropdownItems()}</Dropdown.Menu>
+        </Dropdown>
+        <Form.Group controlId="searchTerm">
+          <Form.Control
+            type="text"
+            placeholder="Search by title"
+            value={searchTerm}
+            onChange={handleSearchTermChange}
+          />
+        </Form.Group>
         <hr />
         {!games ? (
           <Loader />
